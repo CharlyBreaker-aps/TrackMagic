a657ef9b1226f365a2659ce457ef82766ab097a4
CharlyBreaker-aps <vettacarlos@gmail.com>
2026-04-17 22:49:23 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'charly-tracker-v14';
+const CACHE = 'charly-tracker-v15';
 const FILES = ['./index.html', './manifest.json'];
 self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES))); self.skipWaiting(); });
 self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))); self.clients.claim(); });
