dcaea2c7fce00e0f161ecacd3e7820315d8778e1
CharlyBreaker-aps <vettacarlos@gmail.com>
2026-08-05 08:31:50 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'charly-tracker-v61';
+const CACHE = 'charly-tracker-v62';
 const FILES = ['./', './index.html', './manifest.json', './icons/icon-192.png', './icons/icon-512.png'];
 
 self.addEventListener('install', e => {
